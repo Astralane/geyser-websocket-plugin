a0dc01783124ae3b45ca87b9d5039ca8f6688a1d
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2024-07-19 01:03:27 +0530

    chore: test

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -1,3 +1,4 @@
+use log::info;
 use crate::plugin::GeyserPluginPostgres;
 use solana_geyser_plugin_interface::geyser_plugin_interface::GeyserPlugin;
 
@@ -8,7 +9,9 @@ use solana_geyser_plugin_interface::geyser_plugin_interface::GeyserPlugin;
 /// This function simply allocates a GeyserPluginHook,
 /// and returns a pointer to it as trait GeyserPlugin.
 pub unsafe extern "C" fn _create_plugin() -> *mut dyn GeyserPlugin {
+    info!("Creating plugin");
     let plugin = GeyserPluginPostgres::new("postgres://postgres:postgres@localhost:5432");
+    info!("Plugin created");
     let plugin: Box<dyn GeyserPlugin> = Box::new(plugin);
     Box::into_raw(plugin)
 }
