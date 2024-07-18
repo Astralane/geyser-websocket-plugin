1810e82dd8635a8a607ff07f6e0780ab030bfe8d
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2024-07-19 02:10:15 +0530

    chore: add pool

--- src/client.rs
+++ src/client.rs
@@ -7,12 +7,15 @@ pub struct Client {
 }
 
 impl Client {
-    pub fn new(url: &str) -> Self {
+    pub fn new(url: &str, no_of_workers: u8) -> Self {
         let (sender, recv) = crossbeam_channel::bounded(100);
-        let worker = DBWorker::new(url, recv);
+        let mut worker = DBWorker::new(url, recv);
         //create a runtime for workers
-        //let rt = tokio::runtime::Runtime::new();
-        std::thread::spawn(|| run_service(worker));
+        let rt = tokio::runtime::Runtime::new();
+        for _ in 0..no_of_workers {
+            let worker_tmp = worker.clone();
+            rt.spawn(run_service(worker_tmp)).expect("failed to spawn worker thread");
+        }
         Self { sender }
     }
     pub fn send(&self, message: DBWorkerMessage) -> Result<(), GeyserPluginPostgresError> {
