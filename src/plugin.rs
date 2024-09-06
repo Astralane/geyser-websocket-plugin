a02b428976629f482a8b37d40df0edc699684419
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2024-09-07 04:25:47 +0530

    new scafold

--- src/plugin.rs
+++ src/plugin.rs
@@ -44,9 +44,9 @@ impl GeyserPlugin for GeyserPluginPostgres {
         config_file: &str,
         _is_reload: bool,
     ) -> solana_geyser_plugin_interface::geyser_plugin_interface::Result<()> {
-        info!("on_load: config_file: {:#?}", config_file);
-        let client = Client::new("postgres://postgres:postgres@localhost:5432", 4);
-        self.client = Some(client);
+        // info!("on_load: config_file: {:#?}", config_file);
+        // let client = Client::new("postgres://postgres:postgres@localhost:5432", 4);
+        // self.client = Some(client);
         Ok(())
     }
 
@@ -88,30 +88,31 @@ impl GeyserPlugin for GeyserPluginPostgres {
     ) -> solana_geyser_plugin_interface::geyser_plugin_interface::Result<()> {
         info!("notify_transaction: transaction for {:#?}", slot);
         //get validator for this slot
-        match transaction {
-            ReplicaTransactionInfoVersions::V0_0_2(transaction_info) => {
-
-                info!("sending message to worker {:?}", transaction_info);
-
-                if let Some(client) = self.client.as_ref() {
-                    let res = client.send(transaction_info.index);
-                    if let Err(e) = res {
-                        return Err(GeyserPluginError::Custom(Box::new(e)));
-                    }
-                } else {
-                    return Err(GeyserPluginError::Custom(Box::new(
-                        GeyserPluginPostgresError::GenericError {
-                            msg: "client not found".to_string(),
-                        },
-                    )));
-                }
-
-                Ok(())
-            }
-            _ => Err(GeyserPluginError::Custom(Box::new(
-                GeyserPluginPostgresError::VersionNotSupported,
-            ))),
-        }
+        // match transaction {
+        //     ReplicaTransactionInfoVersions::V0_0_2(transaction_info) => {
+        //
+        //         info!("sending message to worker {:?}", transaction_info);
+        //
+        //         if let Some(client) = self.client.as_ref() {
+        //             let res = client.send(transaction_info.index);
+        //             if let Err(e) = res {
+        //                 return Err(GeyserPluginError::Custom(Box::new(e)));
+        //             }
+        //         } else {
+        //             return Err(GeyserPluginError::Custom(Box::new(
+        //                 GeyserPluginPostgresError::GenericError {
+        //                     msg: "client not found".to_string(),
+        //                 },
+        //             )));
+        //         }
+        //
+        //         Ok(())
+        //     }
+        //     _ => Err(GeyserPluginError::Custom(Box::new(
+        //         GeyserPluginPostgresError::VersionNotSupported,
+        //     ))),
+        // }
+        Ok(())
     }
 
     fn notify_block_metadata(
