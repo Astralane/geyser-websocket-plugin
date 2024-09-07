30ead3fc497a18d65eb8828a475286b1912c5b64
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2024-09-07 14:22:32 +0530

    no idea

--- src/plugin.rs
+++ src/plugin.rs
@@ -148,7 +148,7 @@ impl GeyserPlugin for GeyserPluginWebsocket {
             index: solana_transaction.index as u64,
         };
         let message = ChannelMessage::Transaction(Box::new(transaction));
-        self.notify_clients(message);
+        //self.notify_clients(message);
         Ok(())
     }
 
