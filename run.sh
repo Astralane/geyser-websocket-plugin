41a800a46f52cda024a87229fe25d8cc37bb5283
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2024-09-12 03:30:45 +0530

    feat: bette logs

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 set -ex
 
 cargo build -r
-RUST_LOG=geyser_test_plugin::plugin=info && solana-test-validator --log --geyser-plugin-config config.json
+RUST_LOG=geyser=info && solana-test-validator --log --geyser-plugin-config config.json
